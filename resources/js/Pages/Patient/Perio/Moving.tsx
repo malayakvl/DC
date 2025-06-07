37e41296ef544af47f651bdfb55d0170aa0c2859
malayakvl <malaya.kvl@gmail.com>
2025-06-07 23:09:56 +0300

    Perio moving stage

--- resources/js/Pages/Patient/Perio/Moving.tsx
+++ resources/js/Pages/Patient/Perio/Moving.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { getTeethDiagnozisSelector } from '../../../Redux/Formula/selectors';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTeethDiagnozisSelector, perioDiagnozisSelector } from '../../../Redux/Formula/selectors';
+import { setPerioDiagnoze } from '../../../Redux/Formula';
 
-export default function Moving({ toothNum }) {
-  // const teethDiagnozis = useSelector(getTeethDiagnozisSelector);
-  // const tooth11Diagnozis = teethDiagnozis.tooth11;
+export default function Moving({ toothNum, type }) {
+  const dispatch = useDispatch<any>();
+  const perioDiagnozis = useSelector(perioDiagnozisSelector);
+  const [changeState, setChangeState] = useState(false);
+
+  const setDiagnozis = () => {
+    let stage = 0;
+    if (perioDiagnozis[`tooth${toothNum}`][`moving_st`] === 0)
+      stage = 1;
+    else if (perioDiagnozis[`tooth${toothNum}`][`moving_st`] === 1)
+      stage = 2;
+    else if (perioDiagnozis[`tooth${toothNum}`][`moving_st`] === 2)
+      stage = 3;
+    else if (perioDiagnozis[`tooth${toothNum}`][`moving_st`] === 3)
+      stage = 0;
+
+    perioDiagnozis[`tooth${toothNum}`][`moving_st`] = stage;
+    dispatch(setPerioDiagnoze(perioDiagnozis));
+    setChangeState(!changeState);
+  };
 
   return (
     <>
@@ -18,7 +36,7 @@ export default function Moving({ toothNum }) {
         style={{ width: '100%', height: '20px' }}
         xmlSpace="preserve"
       >
-        <g className="mobility">
+        <g className="mobility" onClick={() => setDiagnozis()}>
           <path
             d="M 0 10 L 100 10 L 100 90 L 0 90 Z"
             fill="rgba(255, 255, 255, 0)"
@@ -26,20 +44,20 @@ export default function Moving({ toothNum }) {
           <path
             className="level1"
             d="M 0 25 L 0 75"
-            stroke="rgba(200, 200, 200, 0.9)"
             strokeWidth="10px"
+            style={{stroke: perioDiagnozis[`tooth${toothNum}`]?.[`moving_st`] >= 1 ? 'red' : 'rgba(200, 200, 200, 0.9)'}}
           ></path>
           <path
             className="level2"
             d="M 45 25 L 45 75"
-            stroke="rgba(200, 200, 200, 0.9)"
             strokeWidth="10px"
+            style={{stroke: perioDiagnozis[`tooth${toothNum}`]?.[`moving_st`] >= 2 ? 'red' : 'rgba(200, 200, 200, 0.9)'}}
           ></path>
           <path
             className="level3"
             d="M 90 25 L 90 75"
-            stroke="rgba(200, 200, 200, 0.9)"
             strokeWidth="10px"
+            style={{stroke: perioDiagnozis[`tooth${toothNum}`]?.[`moving_st`] >= 3 ? 'red' : 'rgba(200, 200, 200, 0.9)'}}
           ></path>
         </g>
       </svg>
