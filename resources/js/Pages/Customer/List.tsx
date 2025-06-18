e9454ceb89a7f58a852a330e88331631e4e3df3d
malayakvl <malaya.kvl@gmail.com>
2025-06-18 06:56:05 +0300

    customer import

--- resources/js/Pages/Customer/List.tsx
+++ resources/js/Pages/Customer/List.tsx
@@ -52,9 +52,9 @@ export default function List({ clinicData, filialData, customerData }) {
               {customerData?.map(item => (
                 <tr className="" key={item.id}>
                   <td style={{ width: '100px' }}>
-                    {item.photo && (
+                    {item.file && (
                       <img
-                        src={`/uploads/patients/${item.photo}`}
+                        src={`/uploads/users/${item.file}`}
                         width={40}
                         className="float-left rounded"
                         height="auto"
