013563aacbacd6314b755f5b101d86b5a60ca9f9
Victroria <malaya.kvl@gmail.com>
2026-02-09 06:32:23 +0200

    working with opening balance and report

---
+++ app/Http/Requests/OpeningBalanceRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class InvoiceUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'invoice_number' => ['required', 'string', 'max:255'],
+            'invoice_date' => ['required', 'date'],
+            'supplier_id' => ['required'],
+            'customer_id' => ['required'],
+            'store_id' => ['required'],
+            'status' => ['required'],
+            'currency_id' => ['required'],
+            'tax_id' => ['required']
+        ];
+    }
+}
